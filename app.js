03f833f5cb2848583177a3bb2e91cce8644779d1
Kevin <kfig21@gmail.com>
2021-11-16 22:19:27 -0500

    initial commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
 const createError = require("http-errors");
+const cors = require("cors");
 
 // import routes
 const userRouter = require("./routes/users");
@@ -22,6 +23,14 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+// cors middleware
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 // user routes
 app.use("/api/auth", authRouter);
