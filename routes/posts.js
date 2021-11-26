22d23d751e27967c3dc311df468e9aae8ec1a7ae
Kevin <kfig21@gmail.com>
2021-11-25 22:31:26 -0500

    adding comments

--- routes/posts.js
+++ routes/posts.js
@@ -82,9 +82,7 @@ router.put("/:id/like", async (req, res) => {
 // get a post
 router.get("/:id", async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
-      .populate("comments")
-      .populate("userId");
+    const post = await Post.findById(req.params.id).populate("comments");
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
