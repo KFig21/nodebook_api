eaa57c571cb66ae5a915362bc7c834f9efd3fc97
Kevin <kfig21@gmail.com>
2021-11-29 13:35:32 -0500

    edit posts

--- models/post.js
+++ models/post.js
@@ -7,6 +7,7 @@ const PostSchema = new mongoose.Schema(
     body: { type: String, max: 500 },
     img: { type: String },
     likes: { type: Array, default: [] },
+    edited: { type: Boolean, default: false },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true }
